0f521c8efbfbfdb8494dd121220a89c60bd0c0ad
banira4649 <banira.game@outlook.jp>
2024-06-02 20:09:30 +0900

    キル判定の持続時間を修正

--- src/Main.php
+++ src/Main.php
@@ -8,6 +8,7 @@ use banira4649\BetterKillJudgement\event\EventListener;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
+use pocketmine\scheduler\TaskHandler;
 use pocketmine\Server;
 
 class Main extends PluginBase{
@@ -16,6 +17,10 @@ class Main extends PluginBase{
      * @var string[] $damagerList
      */
     private array $damagerList = [];
+    /**
+     * @var TaskHandler[] $closureList
+     */
+    private array $closureList = [];
 
     public function onEnable(): void{
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
@@ -31,7 +36,8 @@ class Main extends PluginBase{
 
     public function setDamager(Player $player, Player $damager): void{
         $this->damagerList[$player->getName()] = $damager->getName();
-        $this->getScheduler()->scheduleDelayedTask(new ClosureTask(
+        if(isset($this->closureList[$player->getName()])) $this->closureList[$player->getName()]->cancel();
+        $this->closureList[$player->getName()] = $this->getScheduler()->scheduleDelayedTask(new ClosureTask(
             function () use ($player): void{
                 $this->resetDamager($player);
             }
