f68c23641dbf39331457cc9ee0027564c45b94cb
banira4649 <82045074+banira4649@users.noreply.github.com>
2024-06-18 00:01:49 +0900

    CAUSE_VOIDによってプレイヤーが死亡した際の挙動を変更

--- src/event/EventListener.php
+++ src/event/EventListener.php
@@ -42,17 +42,15 @@ class EventListener implements Listener{
                     $event->cancel();
                     $this->main->resetDamager($entity);
                     (new EntityDamageByEntityEvent($damager, $entity, EntityDamageEvent::CAUSE_ENTITY_ATTACK, $event->getFinalDamage()))->call();
+                    if(!class_exists("NeiroNetwork\AlternativeCoreWars\Main")){
+                        $pos = $entity->getPosition()->add(0, $entity->getFallDistance(), 0);
+                        Main::getInstance()->getScheduler()->scheduleDelayedTask(
+                            new ClosureTask(fn() => $entity->isOnline() && $entity->teleport($pos)),
+                            4
+                        );
+                    }
                 }
             }
         }
-    }
-
-    public function onEntityDeath(EntityDamageEvent $event): void{
-        $entity = $event->getEntity();
-        if(!$entity instanceof Player) return;
-        if($event->getCause() !== EntityDamageEvent::CAUSE_VOID) return;
-        if($entity->isAlive() && $entity->getHealth() - $event->getFinalDamage() > 0) return;
-        $pos = $entity->getPosition()->add(0, $entity->getFallDistance(), 0);
-        Main::getInstance()->getScheduler()->scheduleDelayedTask(new ClosureTask(fn() => $entity->isOnline() && $entity->teleport($pos)), 4);
     }
 }
